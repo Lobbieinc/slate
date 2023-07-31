07aa3f53d78a15cdbb5512b9273ca5f99d9ca1c5
dcordz <17937472+dcordz@users.noreply.github.com>
2023-07-31 12:15:45 -0400

    retrived updated index.html.md

--- deploy.sh
+++ deploy.sh
@@ -5,8 +5,8 @@ me=$(basename "$0")
 cp $LOBBIE_PROJECT_DIR/docker/volumes/slate/source/index.html.md ./source/index.html.md
 cp $LOBBIE_PROJECT_DIR/docker/volumes/slate/source/images/logo.png ./source/images/logo.png
 git add -u
-git commit -n -m "retrived updated index.html.md"
-git push origin main
+git commit -n -m "retrived updated index.html.md" | true
+git push origin main | true
 
 help_message="\
 Usage: $me [-c FILE] [<options>]
